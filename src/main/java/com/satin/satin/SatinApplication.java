39e7036dccee203f62c74b6b5d4cb062afd494f9
ArlletGod <arlletgod@gmail.com>
2023-04-18 19:45:57 +0300

    first commit

---
+++ src/main/java/com/satin/satin/SatinApplication.java
@@ -0,0 +1,13 @@
+package com.satin.satin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SatinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/satin/satin/SatinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satin.satin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
